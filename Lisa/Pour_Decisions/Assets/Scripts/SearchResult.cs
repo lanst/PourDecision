37b816f70d2cb034f7f417394dfb83aee49ae0ae
lanst14 <hunter_14_hunter@hotmail.com>
2018-03-30 12:48:31 -0700

    added and modify search functionality

---
+++ Lisa/Pour_Decisions/Assets/Scripts/SearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchResult : MonoBehaviour {
+	public Text result;
+	// Use this for initialization
+	void Start () {
+		result.text = ShowErrorScript.searchResult;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Lisa/Pour_Decisions/Assets/Scripts/ShowErrorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowErrorScript : MonoBehaviour {
+
+	public InputField textField;
+	public Text error;
+	public Button searchBtn;
+	static public string searchResult;
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		error.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		searchBtn.onClick.AddListener(CheckTextField);
+	}
+
+	// Update is called once per frame
+	void CheckTextField() {
+		if (textField.text == "") {
+			error.gameObject.SetActive (true);
+		} 
+		else {
+			error.gameObject.SetActive (false);
+			searchResult = textField.text.ToString();
+			SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		}
+	}
+}
